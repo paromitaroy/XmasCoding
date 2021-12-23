e8270ed65b379910a108f5dd5a8d38938c624bcf
Gordon Byers <gordon.byers@microsoft.com>
2021-12-23 16:23:37 +0000

    Update pwsh-solution.ps1

--- pwsh-solution.ps1
+++ pwsh-solution.ps1
@@ -5,7 +5,4 @@ $arm1params.keys | % {$arm1paramList += New-Object PSObject -Property ([Ordered]
 $arm2paramList = @()
 $arm2params.keys | % {$arm2paramList+= New-Object PSObject -Property ([Ordered]@{Parameter=$_; DefaultValue=$arm2params.Get_Item($_).defaultValue | ConvertTo-Json -Compress })}
 $comparison = Compare-Object $arm1paramList $arm2paramList -Property Parameter, DefaultValue -PassThru | select Parameter, DefaultValue, @{N='FileVersion';E={$_.SideIndicator.replace('<=','Old').replace('=>','New')}}
-
-if ($comparison.length -gt 0) {
-    [string]$html = $comparison | sort-object Parameter, @{e='FileVersion';desc=$true} | ConvertTo-Html -Fragment | Out-File "ghpr.html"
-}
+[string]$html = $comparison | sort-object Parameter, @{e='FileVersion';desc=$true} | ConvertTo-Html -Fragment | Out-File "pwsh-differences.html"
